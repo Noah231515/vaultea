5312b3c52bbc37093d8d99f453b3ff90477ce8c0
Noah <noah231515@gmail.com>
2021-09-28 08:19:21 -0400

    generate signup component

---
+++ vaulteaclient/src/app/authentication/sign-up/sign-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vaultea-sign-up',
+  styleUrls: ['./sign-up.component.scss'],
+  templateUrl: './sign-up.component.html',
+})
+export class SignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

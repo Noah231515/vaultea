8ed28b206744f4473dc5860d89a0403eb9110245
Noah <noah231515@gmail.com>
2021-09-28 08:41:39 -0400

    set up event emitters

--- vaulteaclient/src/app/ui-kit/form-footer/form-footer.component.ts
+++ vaulteaclient/src/app/ui-kit/form-footer/form-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BaseComponent } from 'src/app/abstract';
 
 import { ButtonInterface } from '..';
@@ -12,10 +12,22 @@ export class FormFooterComponent extends BaseComponent implements OnInit {
   @Input() public primaryButton: ButtonInterface = this.BUTTONS_CONSTANT.SUBMIT_BUTTON;
   @Input() public secondaryButton: ButtonInterface = this.BUTTONS_CONSTANT.CANCEL_BUTTON;
 
+  @Output() public primaryButtonClicked: EventEmitter<void> = new EventEmitter<void>();
+  @Output() public secondaryButtonClicked: EventEmitter<void> = new EventEmitter<void>();
+
+
   constructor() { 
     super()  
   }
 
   public ngOnInit(): void {
+  }
+
+  public emitPrimaryButtonClicked(): void {
+    this.primaryButtonClicked.emit();
+  }
+
+  public emitSecondaryButtonClicked(): void {
+    this.secondaryButtonClicked.emit();
   }
 }

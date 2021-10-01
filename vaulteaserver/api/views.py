5bdc073a7008969eec03b95c29c904fc30080e60
Noah <noah231515@gmail.com>
2021-09-30 23:20:36 -0400

    happy path sign up works

--- vaulteaserver/api/views.py
+++ vaulteaserver/api/views.py
@@ -1,8 +1,11 @@
-
-from django.shortcuts import render
+from django.http.response import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from api.serializers import SignUpFormSerializer
+import json
 
 # # Create your views here.
 # @api_view(['GET', 'POST', 'DELETE'])
@@ -27,6 +30,11 @@ from rest_framework.response import Response
 
 @api_view(['POST'])
 def sign_up(request):
-  print('hello')
-  print(request)
-  return Response({'key': 'value'}, status=status.HTTP_200_OK)
+  serializer = SignUpFormSerializer(data=json.loads(request.body))
+  if serializer.is_valid():
+    data = serializer.data
+    new_user = User.objects.create(username=data['username'], password=data['password'], email=data['email'])
+    token = Token.objects.create(user=new_user)
+    return JsonResponse({'token': token.key}, status=status.HTTP_200_OK)
+  else:
+    return Response({'key': 'value'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)

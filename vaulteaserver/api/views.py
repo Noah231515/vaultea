5fc04ac7717ba555f13cfcff78f82eba39927ef0
Noah <noah231515@gmail.com>
2021-10-02 10:51:32 -0400

    remove old views

--- vaulteaserver/api/views.py
+++ vaulteaserver/api/views.py
@@ -7,27 +7,6 @@ from rest_framework.authtoken.models import Token
 from api.serializers import SignUpFormSerializer
 import json
 
-# # Create your views here.
-# @api_view(['GET', 'POST', 'DELETE'])
-# def tutorial_list(request):
-#     # GET list of tutorials, POST a new tutorial, DELETE all tutorials
- 
- 
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def tutorial_detail(request, pk):
-#     # find tutorial by pk (id)
-#     try: 
-#         tutorial = Tutorial.objects.get(pk=pk) 
-#     except Tutorial.DoesNotExist: 
-#         return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND) 
- 
-#     # GET / PUT / DELETE tutorial
-    
-        
-# @api_view(['GET'])
-# def tutorial_list_published(request):
-#     # GET all published tutorials
-
 @api_view(['POST'])
 def sign_up(request):
   serializer = SignUpFormSerializer(data=json.loads(request.body))

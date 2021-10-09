fd5463113952dabb583c1a7e8b827091f72fa967
Noah <noah231515@gmail.com>
2021-10-09 19:15:40 -0400

    cleanup

--- vaulteaclient/src/app/services/crypto.service.ts
+++ vaulteaclient/src/app/services/crypto.service.ts
@@ -10,8 +10,6 @@ export class CryptoService {
   public async generateMasterKey(password: string, salt: string): Promise<string> {
     const saltBuffer = CryptoUtil.stringToArrayBuffer(salt);
     const passwordBuffer = CryptoUtil.stringToArrayBuffer(password);
-    console.log(password);
-    console.log(salt);
 
     const pbkdf2Params: Pbkdf2Params = {
       hash: "SHA-256",
@@ -22,7 +20,7 @@ export class CryptoService {
 
     const importKey = await crypto.subtle.importKey("raw", passwordBuffer, { name: "PBKDF2"}, false, ["deriveBits"]);
     const derivedBits = await crypto.subtle.deriveBits(pbkdf2Params, importKey, 256);
-    return CryptoUtil.arrayBufferToBase64(derivedBits);
+    return CryptoUtil.arrayBufferToAscii(derivedBits);
   }
 
   public async stretchMasterKey(masterKey: string, salt: string): Promise<string> {
--- vaulteaclient/src/app/utils/crypto.util.ts
+++ vaulteaclient/src/app/utils/crypto.util.ts
@@ -1,30 +1,31 @@
 export class CryptoUtil {
 
   public static stringToArrayBuffer(string: string): ArrayBuffer {
-    const resultBuffer = new ArrayBuffer(string.length * 2);
-    const bufferView = new Uint16Array(resultBuffer);
+    // https://stackoverflow.com/questions/30631927/converting-to-base64-in-javascript-without-deprecated-escape-call
+    // Helps translate characters outside of utf8 to inside of utf8 to then be converted to ascii
+    const cleanedString = unescape(encodeURIComponent(string));
+    const bufferView = new Uint8Array(string.length);
     for (let i = 0; i < string.length; i++) {
-      bufferView[i] = string.charCodeAt(i);
+      bufferView[i] = cleanedString.charCodeAt(i);
     }
     return bufferView;
   }
 
-  public static arrayBufferToBase64(buffer: ArrayBuffer): string {
-    const intArray = new Uint8Array(buffer); // Values are converted from binary to bytes to decimal
-    console.log(intArray);
+  public static arrayBufferToAscii(buffer: ArrayBuffer): string {
+    const byteArray = new Uint8Array(buffer); // Raw binary data is chunked into bytes, stored as decimal representation
     let result = "";
-    for (let i = 0; i < intArray.length; i++) {
-      result += String.fromCharCode(intArray[i]); // Converts decimal values to utf8
+    for (let i = 0; i < byteArray.length; i++) {
+      result += String.fromCharCode(byteArray[i]); // Appends each ascii glyph from raw decimal data
     }
-    return btoa(result); // Converts utf8 to base64
+    return btoa(result);
   }
 
   private buffToString(buff: ArrayBuffer): void {
     
     const intArray = new Uint8Array(buff);
-    console.log("from buff");
-    console.log(
-      String.fromCharCode.apply(null, intArray as any).toString()
-    );
+    // console.log("from buff");
+    // console.log(
+    //   String.fromCharCode.apply(null, intArray as any).toString()
+    // );
   }
 }

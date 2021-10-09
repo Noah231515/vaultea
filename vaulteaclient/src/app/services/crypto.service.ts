10ffa258ecb7c9f1b3b10afa62d2d19e039b703c
Noah <noah231515@gmail.com>
2021-10-09 16:16:25 -0400

    wip

--- vaulteaclient/src/app/services/crypto.service.ts
+++ vaulteaclient/src/app/services/crypto.service.ts
@@ -9,7 +9,9 @@ export class CryptoService {
 
   public async test(password: string, salt: string): Promise<any> {
     const saltBuffer = CryptoUtil.stringToArrayBuffer(salt);
-    const passwordBuffer = CryptoUtil.stringToArrayBuffer(password)
+    const passwordBuffer = CryptoUtil.stringToArrayBuffer(password);
+    this.buffToString(saltBuffer);
+
 
     const pbkdf2Params = {
       iterations: 100000,
@@ -20,7 +22,28 @@ export class CryptoService {
 
     const importKey = await crypto.subtle.importKey("raw", passwordBuffer, { name: "PBKDF2"}, false, ["deriveBits"]);
     console.log(importKey);
-    const deriveBits = await crypto.subtle.deriveBits(pbkdf2Params, importKey, 512);
-    console.log(deriveBits);
+    const derivedBits = await crypto.subtle.deriveBits(pbkdf2Params, importKey, 256);
+    this.arrayBufferToBase64(derivedBits);
+  }
+  
+  private buffToString(buff: ArrayBuffer): void {
+    
+    const intArray = new Uint8Array(buff);
+    console.log("from buff");
+    console.log(
+      String.fromCharCode.apply(null, intArray as any).toString()
+    );
+  }
+
+  private arrayBufferToBase64(buffer: ArrayBuffer): void {
+    const intArray = new Uint8Array(buffer); // Values are converted from binary to bytes to decimal
+    let result = "";
+    for (let i = 0; i < intArray.length; i++) {
+      console.log(intArray[i]);
+      result += String.fromCharCode(intArray[i]); // Converts decimal values to utf8
+    }
+    console.log(result);
+    console.log(btoa(result)); // Converts utf8 to base64
+
   }
 }

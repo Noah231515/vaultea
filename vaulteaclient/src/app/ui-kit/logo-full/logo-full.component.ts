83fe918124e5671d2d2886f4aad6478a46f35b15
Noah <noah231515@gmail.com>
2021-10-17 17:56:58 -0400

    new lines, remove uneeded file

--- vaulteaclient/src/app/ui-kit/logo-full/logo-full.component.ts
+++ vaulteaclient/src/app/ui-kit/logo-full/logo-full.component.ts
@@ -3,7 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "vaultea-logo-full",
   templateUrl: "./logo-full.component.html",
-  styleUrls: ["./logo-full.component.scss"]
 })
 export class LogoFullComponent {
 }

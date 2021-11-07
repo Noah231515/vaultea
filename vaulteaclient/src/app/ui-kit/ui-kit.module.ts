48286d5f4b81eebc8e59b4de5f25e87f085e6964
Noah <noah231515@gmail.com>
2021-11-07 10:25:23 -0500

    some card set up

--- vaulteaclient/src/app/ui-kit/ui-kit.module.ts
+++ vaulteaclient/src/app/ui-kit/ui-kit.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
@@ -42,6 +43,7 @@ import { LogoFullComponent } from "./logo-full/logo-full.component";
     MatIconModule,
     MatInputModule,
     ReactiveFormsModule,
+    MatCardModule,
   ]
 })
 export class UiKitModule { }

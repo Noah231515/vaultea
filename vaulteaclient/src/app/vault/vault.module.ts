a5a4a5be2ae2a896a13b8acae5ab72a3e2b8f598
Noah <noah231515@gmail.com>
2021-11-07 13:53:03 -0500

    styling changes, grid

--- vaulteaclient/src/app/vault/vault.module.ts
+++ vaulteaclient/src/app/vault/vault.module.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 import { UiKitModule } from "../ui-kit/ui-kit.module";
 import { VaultRoutingModule } from "./vault-routing.module";
-import { VaultComponent } from './vault/vault.component';
+import { VaultComponent } from "./vault/vault.component";
 
 @NgModule({
   declarations: [
-    VaultComponent
+    VaultComponent,
   ],
   imports: [
     CommonModule,
+    MatGridListModule,
     UiKitModule,
-    VaultRoutingModule
+    VaultRoutingModule,
   ]
 })
 export class VaultModule { }

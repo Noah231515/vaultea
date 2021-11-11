8e9a7eb25a253f05a5583fb4846af67e9436a6b9
Noah <noah231515@gmail.com>
2021-11-11 09:49:47 -0500

    generate keys

--- vaulteaclient/src/app/services/browser-crypto.spec.ts
+++ vaulteaclient/src/app/services/browser-crypto.spec.ts
@@ -1,5 +1,11 @@
+import { TestBed } from "@angular/core/testing";
 import { FormBuilder, FormGroup } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
+import { CRYPTO_SERVICE } from "../abstract";
+import { USER_SERVICE } from "../abstract/tokens/user-service.token";
+import { StretchedMasterKey } from "../utils/stretched-master-key.model";
+import { VaulteaCryptoKey } from "../utils/vaultea-crypto-key.model";
 import { BrowserCryptoService } from "./browser-crypto.service";
 import { BrowserUserService } from "./browser-user.service";
 
@@ -8,19 +14,33 @@ describe("BrowserCryptoService", () => {
   let userService: BrowserUserService;
   let formBuilder: FormBuilder;
   let form: FormGroup;
+  let stretchedMasterKey: StretchedMasterKey;
+  let encryptionKey: VaulteaCryptoKey;
 
-  beforeEach(() => { 
-    userService = new BrowserUserService();
-    cryptoService = new BrowserCryptoService(userService); 
-    formBuilder = new FormBuilder();
+  beforeEach(async () => {
+    TestBed.configureTestingModule({ 
+      imports: [
+        BrowserModule
+      ],
+      providers: [
+        FormBuilder,
+        {provide: USER_SERVICE, useClass: BrowserUserService},
+        {provide: CRYPTO_SERVICE, useClass: BrowserCryptoService},
+      ] 
+    });
+    formBuilder = TestBed.inject(FormBuilder);
+    userService = TestBed.inject(BrowserUserService);
+    cryptoService = TestBed.inject(BrowserCryptoService);
+
     form = formBuilder.group({
       username: "test",
       password: "test"
     });
-    cryptoService.generateKeys(form);
+    stretchedMasterKey = await cryptoService.generateStretchedMasterKey(form.get("password")?.value, form.get("username")?.value);
+    encryptionKey = await cryptoService.generateEncryptionKey();
   });
-
-  it("#getValue should return real value", () => {
+  
+  it("#getValue should return real value", async () => {
     expect(1).toBe(1);
   });
 });

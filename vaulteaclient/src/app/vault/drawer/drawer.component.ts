2e8f1b6ad160c93dce21a927f72ac5a047dd29eb
Noah <noah231515@gmail.com>
2021-11-29 08:53:32 -0500

    add tree

--- vaulteaclient/src/app/vault/drawer/drawer.component.ts
+++ vaulteaclient/src/app/vault/drawer/drawer.component.ts
@@ -1,5 +1,36 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { BaseComponent } from "@abstract";
+import { NestedTreeControl } from "@angular/cdk/tree";
+import { MatTreeNestedDataSource } from "@angular/material/tree";
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
+  },
+  {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
+      },
+      {
+        name: 'Orange',
+        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
+      },
+    ],
+  },
+];
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -8,7 +39,14 @@ import { BaseComponent } from "@abstract";
   templateUrl: "./drawer.component.html",
 })
 export class DrawerComponent extends BaseComponent {
-  constructor() { 
+  public treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  public dataSource = new MatTreeNestedDataSource<FoodNode>();
+  
+  constructor() {
     super();
+    this.dataSource.data = TREE_DATA;
   }
+
+  public hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+  
 }

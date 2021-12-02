02d4a23ce79d906aae80f08c2da71be448e4f2c9
Noah <noah231515@gmail.com>
2021-12-02 08:57:54 -0500

    stub in form

--- vaulteaclient/src/app/vault/drawer/add-folder/add-folder.component.ts
+++ vaulteaclient/src/app/vault/drawer/add-folder/add-folder.component.ts
@@ -1,15 +1,33 @@
+import { BaseComponent } from "@abstract";
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: "vaultea-add-folder",
   templateUrl: "./add-folder.component.html",
   styleUrls: ["./add-folder.component.scss"]
 })
-export class AddFolderComponent implements OnInit {
+export class AddFolderComponent extends BaseComponent implements OnInit {
+  public form: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) { super(); }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.initForm();
   }
 
+  private initForm(): void {
+    this.form = this.formBuilder.group({
+
+    });
+  }
+
+  public submit(): void {
+
+  }
+
+  public cancel(): void {
+    
+  }
 }

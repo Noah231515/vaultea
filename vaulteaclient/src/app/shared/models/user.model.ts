9e234c54f478365919e92c30e6d380c6d258779c
Noah <noah231515@gmail.com>
2021-12-02 08:24:05 -0500

    add vaultId to user

--- vaulteaclient/src/app/shared/models/user.model.ts
+++ vaulteaclient/src/app/shared/models/user.model.ts
@@ -2,4 +2,5 @@ export class User {
   id: string;
   username: string;
   accessToken: string;
+  vaultId: string;
 }
--- vaulteaserver/api/user_service.py
+++ vaulteaserver/api/user_service.py
@@ -1,9 +1,13 @@
+from api.models import User, Vault
+
 class UserService:
   
   @staticmethod
   def get_user_info(user, refreshToken):
+    vault = Vault.objects.filter(user_id=user.id).first()
     return {
       "id": user.id,
       "username": user.username,
-      "accessToken": str(refreshToken.access_token)
+      "accessToken": str(refreshToken.access_token),
+      "vaultId": vault.id
     }

2741e1dfc99078d07dc44f40f151be28a8db37cb
Noah <noah231515@gmail.com>
2021-12-04 14:43:12 -0500

    add description

--- vaulteaserver/api/serializers/folder_serializer.py
+++ vaulteaserver/api/serializers/folder_serializer.py
@@ -4,7 +4,7 @@ from api.models import Folder, Vault
 class FolderSerializer(serializers.Serializer):
     vault_id = serializers.IntegerField(required=True)
     name = serializers.CharField(required=True, max_length=100)
-    #description = serializers.CharField(required=True, max_length=100)
+    description = serializers.CharField(required=False, max_length=100)
     
     def create(self, validated_data):
         vault = Vault()

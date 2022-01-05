e19b0a75326233e3e11918f56d95c4ec5e5f9a25
Noah <noah231515@gmail.com>
2022-01-05 08:14:31 -0500

    stub component

--- vaulteaclient/src/app/ui-kit/ui-kit.module.ts
+++ vaulteaclient/src/app/ui-kit/ui-kit.module.ts
@@ -13,6 +13,7 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { AbstractModule } from "../abstract/abstract.module";
 import { ButtonComponent } from "./button/button.component";
 import { CardComponent } from "./card/card.component";
+import { FolderTreeComponent } from "./folder-tree/folder-tree.component";
 import { FormFooterComponent } from "./form-footer/form-footer.component";
 import { FormHeaderComponent } from "./form-header/form-header.component";
 import { FormComponent } from "./form/form.component";
@@ -28,6 +29,7 @@ import { LogoFullComponent } from "./logo-full/logo-full.component";
     InputComponent,
     LogoFullComponent,
     FormHeaderComponent,
+    FolderTreeComponent,
   ],
   exports: [
     ButtonComponent,

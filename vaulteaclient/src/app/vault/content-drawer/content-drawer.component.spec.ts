f4cba66ad5fcb2911fe2060b54010e6b95d6472a
Noah231515 <44912201+Noah231515@users.noreply.github.com>
2022-01-18 08:37:20 -0500

    Feature/add linting to ci (#37)
    * update yml
    
    * fix a couple of lint errors
    
    * use newer node version
    
    * update package lock
    
    Co-authored-by: Noah <noah231515@gmail.com>

--- vaulteaclient/src/app/vault/content-drawer/content-drawer.component.spec.ts
+++ vaulteaclient/src/app/vault/content-drawer/content-drawer.component.spec.ts
@@ -1,8 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ContentDrawerComponent } from './content-drawer.component';
+import { ContentDrawerComponent } from "./content-drawer.component";
 
-describe('ContentDrawerComponent', () => {
+describe("ContentDrawerComponent", () => {
   let component: ContentDrawerComponent;
   let fixture: ComponentFixture<ContentDrawerComponent>;
 
@@ -10,7 +10,7 @@ describe('ContentDrawerComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ ContentDrawerComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +19,7 @@ describe('ContentDrawerComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });

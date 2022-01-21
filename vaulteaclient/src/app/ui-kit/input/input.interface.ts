76ace0023cf9903485886089c06f7ca59cd8c4c8
Noah <noah231515@gmail.com>
2022-01-21 07:57:23 -0500

    input

---
+++ vaulteaclient/src/app/ui-kit/input/input.interface.ts
@@ -0,0 +1,9 @@
+export interface InputData {
+  formControl: any;
+  label: string;
+  placeholder: string;
+  flexAmount?: string;
+  maxLength?: number;
+  sensitiveDataInput?: boolean;
+  required?: boolean;
+}

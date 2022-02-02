84719c157a1d57f56cb847808461a2466bdbc6e4
Noah <noah231515@gmail.com>
2022-02-02 12:25:52 -0500

    set up serializer

---
+++ vaulteaserver/api/serializers/password_serializer.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from api.models import Folder, Password, Vault
+
+class PasswordSerializer(serializers.Serializer):
+    vault_id = serializers.IntegerField(required=True)
+    folder_id = serializers.IntegerField(required=False, allow_null=True)
+    name = serializers.CharField(required=True, max_length=512)
+    username = serializers.CharField(required=True, max_length=512)
+    password = serializers.CharField(required=True, max_length=512)
+    note = serializers.CharField(required=False, max_length=1000)
+
+    def create(self, validated_data):
+        vault = Vault()
+        vault.id = validated_data['vault_id']
+        validated_data['vault_id'] = vault
+        if (validated_data['folder_id']):
+            folder = Folder()
+            folder.id = validated_data['folder_id']
+            validated_data['folder_id'] = folder
+
+        return Password.objects.create(**validated_data)
+    
+    def update(self, validated_data, folder_id):
+        Folder.objects.filter(id=folder_id).update(**validated_data)
+        return Folder.objects.get(id=folder_id)

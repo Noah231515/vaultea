c3df7e9a106a98f0cf48850276d9706985fd2fd4
Noah <noah231515@gmail.com>
2022-03-01 08:41:40 -0500

    config

---
+++ vaulteaclient/jest.config.ts
@@ -0,0 +1,8 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true,
+  setupFilesAfterEnv: ["./jest-setup.ts"]
+};
+
+module.exports = config;

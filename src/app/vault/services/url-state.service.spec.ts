8119ea190d2cc8656a52566e0073695557bc2dd4
Noah <noah231515@gmail.com>
2022-05-12 09:02:28 -0400

    lint

--- src/app/vault/services/url-state.service.spec.ts
+++ src/app/vault/services/url-state.service.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { UrlStateService } from './url-state.service';
+import { UrlStateService } from "./url-state.service";
 
-describe('UrlStateService', () => {
+describe("UrlStateService", () => {
   let service: UrlStateService;
 
   beforeEach(() => {
@@ -10,7 +10,7 @@ describe('UrlStateService', () => {
     service = TestBed.inject(UrlStateService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });

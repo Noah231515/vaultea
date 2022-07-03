7928606ca1bad4da15360e8cf720a0cb40be3df7
Noah <noah231515@gmail.com>
2022-07-03 17:56:02 -0400

    fix user preference state test

--- src/app/user-preferences/services/user-preference-state.service.spec.ts
+++ src/app/user-preferences/services/user-preference-state.service.spec.ts
@@ -1,16 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import {
+  BrowserCryptoBusinessLogicService, BrowserCryptoFunctionService, CryptoBusinessLogicService,
+  CryptoFunctionService, UserKeyService
+} from "@crypto";
 
-import { UserPreferenceStateService } from './user-preference-state.service';
+import { UserPreferenceStateService } from "./user-preference-state.service";
 
-describe('UserPreferenceStateService', () => {
+describe("UserPreferenceStateService", () => {
   let service: UserPreferenceStateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: UserKeyService },
+        { provide: CryptoFunctionService, useClass: BrowserCryptoFunctionService },
+        { provide: CryptoBusinessLogicService, useClass: BrowserCryptoBusinessLogicService },
+      ]
+    });
     service = TestBed.inject(UserPreferenceStateService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
